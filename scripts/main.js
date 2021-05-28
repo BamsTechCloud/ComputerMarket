2635b3d17d032b0f74b8331d15604a1fe6d1c811
BididCoda <BididCoda2405@gmail.com>
2021-05-28 21:24:17 +0200

    Ajout de fonction setDetais()

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,13 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_IMAGE_TITLE = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl,titleText) {
+    'use strict';
+    var Detail_Image = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    Detail_Image.setAttribute('src',imageUrl);
+    var Detail_Image_Title = document.querySelector(DETAIL_IMAGE_TITLE);
+    Detail_Image_Title.textContent = titleText;
+}
+
+setDetails('images/tech2.jpg','you are King');
